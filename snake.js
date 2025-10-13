f44309628755b84df0bd120b10416ee44419d46d
jayanth-14 <jayanthpetchetii@gmail.com>
2025-10-13 14:05:38 +0530

    added snake board generation

---
+++ snake.js
@@ -0,0 +1,46 @@
+const BOARD = [10, 10];
+// ===== Formatting =====
+function bold(text) {
+  return "\x1B[1m" + text + "\x1B[0m";
+}
+function customBg(text, code) {
+  return "\x1B[48;5;" + code + "m" + text + "\x1B[0m";
+}
+function custom(text, code) {
+  return "\x1B[38;5;" + code + "m" + text + "\x1B[0m";
+}
+function yellow(text) {
+  return "\x1B[33m" + text + "\x1B[0m";
+}
+function generateBoard() {
+  let boardString = "\t";
+  for (let row = 0; row < BOARD[0]; row++) {
+    for (let column = 0; column < BOARD[1]; column++) {
+      boardString += "⬜️";
+    }
+    boardString += "\n\t";
+  }
+  return boardString;
+}
+function space(lines = 1) {
+  console.log("\n".repeat(lines));
+}
+function clear() {
+  console.clear();
+}
+function displayTitle(title, titleColor = 213, borderColor = 33) {
+  const titleString = bold(custom(title, titleColor));
+  const horizontalBorders = customBg(" ".repeat(title.length + 2), borderColor);
+  const verticalBorders = customBg(" ", borderColor);
+  console.log("\t" + horizontalBorders);
+  console.log("\t" + verticalBorders + titleString + verticalBorders);
+  console.log("\t" + horizontalBorders);
+}
+function start() {
+  displayTitle(" 🐉 GROW YOUR DRAGON 🐉 ");
+  space();
+  const board = generateBoard();
+  console.log(board);
+}
+
+start();
