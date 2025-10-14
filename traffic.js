d05ebadc6543447cfb1a43bf80ad773e1175929c
Rafiya <157607173+Rafiya1207@users.noreply.github.com>
2025-10-14 17:33:25 +0530

    add signal numbers

--- traffic.js
+++ traffic.js
@@ -1,3 +1,11 @@
+function red(text) {
+  return "\x1B[31m" + text + "\x1B[0m";
+}
+
+function green(text) {
+  return "\x1B[32m" + text + "\x1B[0m";
+}
+
 function isArray(x) {
   return typeof x === 'object';
 }
@@ -46,14 +54,15 @@ function getSymbol(array, row, column, symbol) {
   return '  ';
 }
 
-function displayJunction(length, signals, roads, vehicle) {
+function displayJunction(length, signals, signalSymbols, roads, vehicle) {
   const half = Math.floor(length / 2);
   let junction = "";
+  let signalCounter = 0;
   for (let i = 0; i < length; i++) {
     for (let j = 0; j < length; j++) {
-      const ts = getSymbol(signals, i, j, '🔴');
+      const ts = getSymbol(signals, i, j, signalSymbols[signalCounter]);
       const vs = getSymbol(roads, i, j, vehicle);
-
+      signalCounter += ts !== "  " ? 1 : 0;
       junction += ts === '  ' ? vs : ts;
     }
     junction += "\n";
@@ -66,8 +75,9 @@ function start() {
   const vehicle = "🚖";
   const roads = [[[1, 2]], [[2, 0]], [[]], [[]]];
   const signals = [[[1, 1]], [[1, 3]], [[3, 3]], [[3, 1]]];
+  const signalNumbers = ["1", "2", "3", "4"];
 
-  displayJunction(length, signals, roads, vehicle);
+  displayJunction(length, signals, signalNumbers, roads, vehicle);
 }
 
 start();
