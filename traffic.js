32a2e1d039808c2b152ebdc80d08cdd24162caea
jayanth-14 <jayanthpetchetii@gmail.com>
2025-10-14 15:01:59 +0530

    implemented display for given length

--- traffic.js
+++ traffic.js
@@ -1,9 +1,43 @@
+function getSymbol(i, j, half) {
+  if (i === 2 && j === 2) {
+    return "  ";
+  }
+  if (i === 2 || j === 2) {
+    return "🚖";
+  }
+  if (i + 1 === half && j + 1  === half) {
+    return "🟩";
+  }
+  if (i - 1 === half && j - 1  === half) {
+    return "🟩";
+  }
+  if (i + 1 === half && j - 1  === half) {
+    return "🟩";
+  }
+  if (i - 1 === half && j + 1  === half) {
+    return "🟩";
+  }
+  return "  ";
+}
+function displayJunction(length) {
+  const half = Math.floor(length / 2);
+  let junction = "";
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      junction += getSymbol(i, j, half);
+    }
+    junction += "\n";
+  }
+  console.log(junction);
+}
 
-function start(){
+function start() {
+  const length = 5;
   const roadOne = ["🚖"];
   const roadTwo = [];
   const roadThree = [];
   const roadFour = [];
+  displayJunction(length);
 }
 
 start();
