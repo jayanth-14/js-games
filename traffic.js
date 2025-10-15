2564c0bc8956572d7df1f2cf7fd6d2b7f59413d6
jayanth-14 <jayanthpetchetii@gmail.com>
2025-10-15 12:14:35 +0530

    implement game loop and format the file

--- traffic.js
+++ traffic.js
@@ -1,5 +1,6 @@
 const VEHICLE = "🚖";
 const SIGNAL_NUMBERS = ["1 ", "2 ", "4 ", "3 "];
+
 function red(text) {
   return "\x1B[31m" + text + "\x1B[0m";
 }
@@ -13,7 +14,7 @@ function bold(text) {
 }
 
 function isArray(x) {
-  return typeof x === 'object';
+  return typeof x === "object";
 }
 
 function areArraysEqual(array1, array2) {
@@ -53,26 +54,30 @@ function includes(array, target) {
 
 function getSymbol(array, row, column, symbol) {
   for (let index = 0; index < array.length; index++) {
-    if (includes(array[index], [row, column])) { 
+    if (includes(array[index], [row, column])) {
       return symbol;
     }
   }
-  return '  ';
+  return "  ";
 }
 
 function toggleSignal(signal, activeSignal) {
-  return signal.trim() === activeSignal + "" ? bold(green(signal)) : bold(red(signal));
+  return signal.trim() === activeSignal + ""
+    ? bold(green(signal))
+    : bold(red(signal));
 }
 
 function displayJunction(length, signals, roads, activeSignal) {
   let junction = "";
   let signalCounter = 0;
+
   for (let i = 0; i < length; i++) {
     for (let j = 0; j < length; j++) {
       const ts = getSymbol(signals, i, j, SIGNAL_NUMBERS[signalCounter]);
       const vs = getSymbol(roads, i, j, VEHICLE);
+
       signalCounter += ts !== "  " ? 1 : 0;
-      junction += ts === '  ' ? vs : toggleSignal(ts, activeSignal);
+      junction += ts === "  " ? vs : toggleSignal(ts, activeSignal);
     }
     junction += "\n";
   }
@@ -82,26 +87,42 @@ function displayJunction(length, signals, roads, activeSignal) {
 function askSignal() {
   return parseInt(prompt("Enter the signal number to open : "));
 }
+
 function getRoadIndex(activeSignal) {
   return (activeSignal + 1) % 4;
 }
-function popVehicles(roads, roadIndex) {  
+
+function popVehicles(roads, roadIndex) {
   roads[roadIndex].splice(0, roads[roadIndex].length);
 }
+
+function areRoadsEmpty(roads) {
+  for (let index = 0; index < roads.length; index++) {
+    if (roads[index].length !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function start() {
   const length = 5;
-  const roads = [[[2, 0]], [[0, 2], [1, 2]], [[2,3], [2,4]], [[3,2]]];
+  const roads = [[[2, 0]], [[0, 2], [1, 2]], [[2, 3], [2, 4]], [[3, 2]]];
   const signals = [[[1, 1]], [[1, 3]], [[3, 3]], [[3, 1]]];
+
   let activeSignal = 0;
+  let isGameOver = false;
+
+  while (!isGameOver) {
+    displayJunction(length, signals, roads, activeSignal);
+    activeSignal = askSignal();
+    const roadIndex = getRoadIndex(activeSignal);
+
+    popVehicles(roads, roadIndex);
+    isGameOver = areRoadsEmpty(roads);
+    console.clear();
+  }
   displayJunction(length, signals, roads, activeSignal);
-  
-  activeSignal = askSignal();
-  const roadIndex = getRoadIndex(activeSignal);
-  console.log(roadIndex);
-  
-  popVehicles(roads, roadIndex);
-  displayJunction(length, signals, roads, activeSignal);
-  console.log(roadIndex);
 }
 
 start();
